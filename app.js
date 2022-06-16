5e065400eb60697e1ee4f7372ecb45fe084c9a00
Collins <ckigen.k@gmail.com>
2022-06-16 17:42:25 +0300

    Connect mongo db

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express =  require('express')
+const express = require('express')
 const app = express()
+const connectDB=require('./db/connect')
+require('dotenv').config()
 
-app.get('/', (req, res)=>{
- res.send('Welcome to Jobs Application Manager')
+app.get('/', (req, res) => {
+  res.send('Welcome to Jobs Application Manager')
 })
 
 const PORT = 5000
 
-app.listen(PORT,()=>{
- console.log(`Server is listening on port ${PORT}...`);
-})
+const start = async()=>{
+  try {
+    await connectDB(process.env['MONGO_URI'])
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}...`)
+    })
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+start()
